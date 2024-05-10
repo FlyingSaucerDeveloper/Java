36db237a0138768a2b04eae794035bebc3dfa41d
Kartik Sharma <official.mrkartiksharma@gmail.com>
2024-05-10 02:39:20 +0100

    Recurssion Problem - Remove Duplicates, move the element(s) to the end of the string

--- Java/JAVA_Placement_Learning/Recursion_Level1_9Problems.java
+++ Java/JAVA_Placement_Learning/Recursion_Level1_9Problems.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Recursion_Level1_9Problems {
 
     public static void towerOfHanoi(int disc, String src, String helper, String Destination) {
@@ -75,6 +77,63 @@ public class Recursion_Level1_9Problems {
 
     }
 
+    public static void elementMover(String s, int l, char element, int i, String ns, int count) {
+
+        // base case
+        if (i == l) {
+            int j = 0;
+            while (j < count) {
+                ns = ns + element;
+                j++;
+
+            }
+            System.out.println("Final String : " + ns + " and string lenght is : " + ns.length());
+            return;
+        }
+
+        if (s.charAt(i) != element) {
+
+            ns = ns + s.charAt(i);
+        }
+
+        else {
+            count++;
+        }
+
+        elementMover(s, l, element, i + 1, ns, count);
+
+    }
+
+    public static void dupRemover(String s, int i, int j, String ns, String[] arr) {
+
+        if (i == s.length() - 1) {
+            System.out.println("Final String : " + ns);
+            return;
+
+        }
+
+        /*
+         * Arrays.asList(yourArray).contains(yourValue)
+         * Warning: this doesn't work for arrays of primitives
+         * 
+         */
+
+        // reference :
+        // https://stackoverflow.com/questions/1128723/how-do-i-determine-whether-an-array-contains-a-particular-value-in-java
+
+        if (!(Arrays.stream(arr).anyMatch((s.charAt(i) + "")::equals))) {
+            if (s.charAt(i) != s.charAt(j)) {
+
+                ns = ns + s.charAt(i);
+
+                arr[i] = s.charAt(i) + "";
+            }
+        }
+
+        dupRemover(s, i + 1, j + 1, ns, arr);
+
+    }
+
     public static void main(String[] args) {
 
         // Tower of Hanoi
@@ -93,7 +152,7 @@ public class Recursion_Level1_9Problems {
 
         // Find the 1st and last occurance of an element in string
         x = "baabcdafghbb";
-        char find = 'c';
+        char find = 'a';
         System.out.println("Find the 1st and last occurance of -" + find + "- in string : " + x);
         occurenceFinder(x, find, 0, -1, -1);
         System.out.println("\n");
@@ -102,7 +161,21 @@ public class Recursion_Level1_9Problems {
         System.out.println("check if the array is sorted (Strickly Increasing)");
         int xArr[] = { -1, 0, 2, 4, 5, 8 };
         sortChecker(xArr, 0);
-        System.out.println("\n");
+        System.out.println();
+
+        // move all elements to the end of the string
+        String s = "axbcxxd";
+        char element = 'x';
+        System.out.println("move element(s) --" + element + "-- to the end of the string : " + s);
+        elementMover(s, s.length(), element, 0, "", 0);
+        System.out.println();
+
+        // Remove duplicates from the string
+        String sO = "Kartik Sharma";
+        System.out.println("Remove the duplicates from the string : " + sO);
+        String[] arr = new String[sO.length()];
+        dupRemover(sO.toLowerCase(), 0, 1, "", arr);
+        System.out.println();
 
     }
 
