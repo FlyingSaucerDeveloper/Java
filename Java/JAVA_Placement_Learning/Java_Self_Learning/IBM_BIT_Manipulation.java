e057be99b9248e837bcb27c021b385ffecd96aa1
Kartik Sharma <official.mrkartiksharma@gmail.com>
2024-06-15 00:32:05 +0100

    IBM BIT MAnipulation - 2 Approach

--- Java/JAVA_Placement_Learning/Java_Self_Learning/IBM_BIT_Manipulation.java
+++ Java/JAVA_Placement_Learning/Java_Self_Learning/IBM_BIT_Manipulation.java
@@ -47,6 +47,8 @@ The third line contains a string of length bits, the binary representation of x.
 
 public class IBM_BIT_Manipulation {
 
+    Set yCombos = new HashSet<String>();
+
     // **** First Approach, only works with maxSet = 1 ****
     @SuppressWarnings("unchecked")
     public void helper(int bits, int maxSet, int x, int y, List answer, int i, int maxY, int count,
@@ -95,24 +97,69 @@ public class IBM_BIT_Manipulation {
         return Integer.toBinaryString((int) answer.get(0));
     }
 
-    
-    @SuppressWarnings("rawtypes")
-    public String findYValueSecondApproach(int bits, int maxSet, String x) {
-        int y = 0;
-        int xo = Integer.parseInt(x, 2);
-        System.out.println("Value of X : " + xo);
-        System.out.println();
-        List answer = new ArrayList<Integer>();
-        //helper(bits, maxSet, xo, y, answer, 0, 0, 0, 0);
-        return Integer.toBinaryString((int) answer.get(0));
+    @SuppressWarnings("unchecked")
+    public void calcYcombinations(int maxValue) {
+
+        if (maxValue == 0) {
+            yCombos.add(Integer.toBinaryString(maxValue));
+            return;
+        }
+
+        yCombos.add(Integer.toBinaryString(maxValue));
+        calcYcombinations(maxValue - 1);
+
     }
 
+    public String helper2(int i, String x, int maxValue, String answer) {
 
+        for (Object y : yCombos) {
+
+            int XORResult = Integer.parseInt((String) y) ^ Integer.parseInt(x);
+            System.out.println((String) y + " XOR " + x + " : -> " + XORResult);
+
+            if (XORResult > maxValue) {
+                maxValue = XORResult;
+                answer = (String) y;
+            }
+
+        }
+
+        return answer;
+    }
+
+    @SuppressWarnings("unchecked")
+    public String findYValueSecondApproach(int bits, int maxSet, String x) {
+
+        int maxValue = 0;
+        int xo = Integer.parseInt(x, 2);
+
+        yCombos.add(Integer.toBinaryString(0));
+
+        // MAx valsue = Summation of 2 to power (Bits -1 -i) under the limits 0 to
+        // maxset-1
+        for (int i = 0; i < maxSet; i++) {
+
+            maxValue = (int) (maxValue + Math.pow(2, bits - 1 - i));
+        }
+
+        System.out.println("MAx Value : " + maxValue);
+
+        calcYcombinations(maxValue);
+
+        System.out.println("all combinations of Y : ");
+
+        for (Object y : yCombos) {
+            System.out.print(y);
+            System.out.print(" ; ");
+        }
+
+        return helper2(0, x, 0, "");
+    }
 
     public static void main(String[] args) {
 
         System.out.println("The y value which resulted in maximal XOR Result is : "
-                + new IBM_BIT_Manipulation().findYValueFirstApproach(4, 1, "1011"));
+                + new IBM_BIT_Manipulation().findYValueSecondApproach(3, 1, "101"));
         System.out.println();
 
     }
